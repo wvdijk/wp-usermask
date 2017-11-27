86844b39caee6ed0b48561b04eb87a5223d0917c
Wieland van Dijk <wvdijk@gmail.com>
2017-11-27 20:58:19 +0100

    Changed scope of checkboxes check

--- content.js
+++ content.js
@@ -3,16 +3,17 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
       (function(){
         // actual code comes here
-        const url = window.location.toString();
-        if (url.indexOf('user-edit.php') > -1) {
+        const URL = window.location.toString();
+        if (URL.indexOf('user-edit.php') > -1) {
           console.log('Applying mask');
           // uncheck all blogs
-          let checkboxes = document.querySelectorAll("input[type='checkbox']");
+          let checkboxes = document.getElementById('profile-div').querySelectorAll("input[type='checkbox']");
           let blogs = ['1','8', '114', '127']; // nieuwshonk, inbeeld, widgets, bvhw
           checkboxes.forEach(function(i) {
             blogs.indexOf(i.value) > -1 ? i.checked = true : i.checked = false;
           });
           document.getElementById('role').value = 'editor';
+          document.getElementById('rich_editing').checked = false;
         } else {
           return;
         }
